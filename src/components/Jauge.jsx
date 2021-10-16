afbbd8c1be8b575cf5746239a0a4a5b631d59c0e
Geoffroy Periot <geoffroy.periot@wildcodeschool.fr>
2021-10-16 15:30:04 +0200

    custom jauges

--- src/components/Jauge.jsx
+++ src/components/Jauge.jsx
@@ -1,12 +1,33 @@
-import './jauge.module.css'
+import './jauge.module.css';
 
-function Jauge({ progress, type, color }) {
-  return (
-    <svg className="jauge" height={200} width={200} xmlns='http://www.w3.org/2000/svg'>
-      <rect stroke="black" stroke-width="3" x='0' y='0' width={26} height={100} fill="none"/>
-      <rect  x='3' y={100-progress} width={20} height={progress} fill="red" />
-    </svg>
-  );
+function Jauge({ progress, visible = true, type, color = 'red' }) {
+  if (visible) {
+    return (
+      <svg
+        className='jauge'
+        height={200}
+        width={200}
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <rect
+          stroke='black'
+          stroke-width='3'
+          x='0'
+          y='0'
+          width={26}
+          height={100}
+          fill='none'
+        />
+        <rect
+          x='3'
+          y={100 - progress}
+          width={20}
+          height={progress}
+          fill={color}
+        />
+      </svg>
+    );
+  }
 }
 
 export default Jauge;

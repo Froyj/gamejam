783aaaf5cbe9d5368601f9bc66067a6b60dd657b
Tbiarneix <thomas.biarneix@gmail.com>
2021-10-17 00:18:25 +0200

    app.js création des div par chapitre / app.css ajustement des classes / ajout des backgrounds / centrage des jauges

--- src/data/landing.js
+++ src/data/landing.js
@@ -1,5 +1,5 @@
 const landing = {
-    title: "Titre du jeu",
+    title: "Titre de la mission",
     text: "Bonjour agent 24601, voici votre nouvelle mission. La planète NH-781 dont vous vous rapprochez ne répond plus à nos appels depuis plusieurs temps déjà. Nous voulons profiter de votre passage dans leur système afin de vous envoyer enquêter pour comprendre pourquoi. C'est une mission dont nous ne connaissons absolument pas la difficulté. Vous pourriez vous retrouver dans une situation très simple comme particulièrement complexe. Cependant nous prenons quand même le risque. Nous avons configuré les coordonnées sur votre tableau de bord. Préparez-vous, c'est parti !",
     button: "Commencer la mission",
 }

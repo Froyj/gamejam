a85282cdd56e1497ab95a98edc7935b6592bb851
Geoffroy Periot <geoffroy.periot@wildcodeschool.fr>
2021-10-16 15:31:37 +0200

    zbleuh

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Jauge from './components/Jauge';
 function App() {
   return (
     <div className="App">
-      <Jauge progress={50}/>
-      <Jauge progress={80}/>
+      <Jauge progress={50} visible color="green"/>
+      <Jauge progress={80} color="blue"/>
       <Jauge progress={50}/>
       <Jauge progress={24}/>
       <Jauge progress={30}/>

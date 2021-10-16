7714f7a0c59d1fc3d2ef5a80a718867e328dd72a
Geoffroy Periot <geoffroy.periot@wildcodeschool.fr>
2021-10-16 22:09:34 +0200

    chapter 1 wip

--- src/data/scenario.js
+++ src/data/scenario.js
@@ -5,9 +5,9 @@ const chapters = [
     name: "The beginning",
     event: [
       {
-        id: 1,
+        id: 0,
         text: "Vous vous approchez de la planète NH-781. Voulez-vous tenter de les contacter ?",
-        img: "",
+        img: "/assets/radio.jpg",
         location: "A bord du vaisseau",
         endOfChapter: false,
         answers: [
@@ -21,37 +21,76 @@ const chapters = [
             text: "Non",
             gaugeType: "fuel",
             mod: -5,
+            nextEvent: 0
+          }
+        ]
+      },
+      {
+        id: 1,
+        text: "L'IA de votre vaisseau vous rappelle que le but de la mission est de comprendre ce qui s'est passé sur cette colonie. Voulez-vous tenter de les contacter ?",
+        img: "/assets/hal.png",
+        location: "A bord du vaisseau",
+        endOfChapter: false,
+        answers: [
+          {
+            text: "Oui",
+            gaugeType: "fuel",
+            mod: -5,
             nextEvent: 2
+          },
+          {
+            text: "Non",
+            gaugeType: "fuel",
+            mod: -5,
+            nextEvent: 0
           }
         ]
       },
       {
         id: 2,
         text: "Pas de réponses, voulez vous retenter votre chance ?",
-        img: "",
-        location: "",
+        img: "/assets/radio.jpg",
+        location: "A bord du vaisseau",
         answers: [
           {
-            id: 2.1,
             text: "Insister",
-            gaugeType: "health",
-            mod: 20,
+            gaugeType: "fuel",
+            mod: -5,
+            nextEvent: 3
           }, {
-            id: 2.2,
             text: "Continuer de survoler la zone en cherchant des indices",
             gaugeType: "fuel",
             mod: -10,
+            nextEvent: 4
           }]
       },
       {
         id: 3,
-        text: "...",
+        text: "Le bruissement de fond émis par la radio semble s'interrompre et vous avez l'impression d'entendre quelqu'un respirer mais personne ne s'exprime",
         img: "",
-        location: "",
+        location: "A bord du vaisseau",
         answers: [
           {
             id: 3.1,
-            text: "...",
+            text: "Tenter d'atterrir par ici",
+            gaugeType: null,
+            mod: -5,
+          }, {
+            id: 3.2,
+            text: "Continuer de survoler la zone",
+            gaugeType: "fuel",
+            mod: -15,
+          }]
+      },
+      {
+        id: 4,
+        text: "Au bout de quelques minutes de survol de la zone, vous finissez par recevoir un signal émis par une balise de détresse",
+        img: "",
+        location: "A bord du vaisseau",
+        answers: [
+          {
+            id: 3.1,
+            text: "Atterrir ici",
             gaugeType: "oxygen",
             mod: -30,
           }, {
@@ -60,7 +99,7 @@ const chapters = [
             gaugeType: "health",
             mod: 0,
           }]
-      }
+      },
     ]
   },
 ]

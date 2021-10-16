88c80930fdc3abda914729cb91da02e7af2f2929
Geoffroy Periot <geoffroy.periot@wildcodeschool.fr>
2021-10-16 12:22:45 +0200

    debut jauges

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Jauge from './components/Jauge';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jauge progress={50}/>
+      <Jauge progress={80}/>
+      <Jauge progress={50}/>
+      <Jauge progress={24}/>
+      <Jauge progress={30}/>
     </div>
   );
 }
